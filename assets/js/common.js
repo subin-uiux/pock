1658310e9ce08fe735e1ac2914bb07e6793f6bd4
leejiwoo924 <miya69406@gmail.com>
2026-09-17 00:23:14 +0900

    디자인 시스템 완료
    완료

--- assets/js/common.js
+++ assets/js/common.js
@@ -28,13 +28,49 @@
     document.documentElement.setAttribute("data-breakpoint", POCK.breakpoint());
   };
 
+  POCK.initFriendScroll = function () {
+    $(".pock-window--friend .pock-window__pane").each(function () {
+      var pane = this;
+      var $pane = $(pane);
+      var $list = $pane.children(".pock-window__list");
+      var $track = $pane.children(".pock-window__scroll");
+      var $thumb = $track.children(".pock-window__scroll-thumb");
+      var list = $list.get(0);
+      var track = $track.get(0);
+      var thumb = $thumb.get(0);
+
+      if (!list || !track || !thumb) {
+        return;
+      }
+
+      function update() {
+        var styles = window.getComputedStyle(list);
+        var padTop = parseFloat(styles.paddingTop) || 0;
+        var padBottom = parseFloat(styles.paddingBottom) || 0;
+
+        track.style.top = padTop + "px";
+        track.style.bottom = padBottom + "px";
+        track.style.left = "";
+        thumb.style.height = "58px";
+        thumb.style.top = "0px";
+      }
+
+      pane._friendScroll = { update: update };
+      update();
+    });
+  };
+
   if (window.gsap && window.ScrollTrigger) {
     gsap.registerPlugin(ScrollTrigger);
   }
 
   $(function () {
     POCK.setBreakpoint();
-    $(window).on("resize.pock", POCK.setBreakpoint);
+    POCK.initFriendScroll();
+    $(window).on("load.pock resize.pock", function () {
+      POCK.setBreakpoint();
+      POCK.initFriendScroll();
+    });
   });
 
   if (typeof motionQuery.addEventListener === "function") {
